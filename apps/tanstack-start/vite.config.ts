9293a3a59a86d8fa9efac7bd8d8a3a172237b476
Edward <me@edwardbock.de>
2025-12-13 22:26:39 +0100

    use nitro for tanstack

--- apps/tanstack-start/vite.config.ts
+++ apps/tanstack-start/vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
 import {tanstackStart} from "@tanstack/react-start/plugin/vite";
-
+import { nitro } from 'nitro/vite'
 export default defineConfig({
     server: {
         port: 3000,
@@ -14,6 +14,7 @@ export default defineConfig({
             projects: ['./tsconfig.json'],
         }),
         tanstackStart(),
+	      nitro(),
         viteReact(),
     ],
 })
